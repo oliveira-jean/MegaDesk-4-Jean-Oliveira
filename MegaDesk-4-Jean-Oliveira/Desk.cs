1d7301a62c043d88d20b43ee3aa906d37d36602f
Jean Oliveira <jeanoliveiracit@gmail.com>
2017-10-07 23:30:44 -0300

    Desk Class coded

--- MegaDesk-4-Jean-Oliveira/Desk.cs
+++ MegaDesk-4-Jean-Oliveira/Desk.cs
@@ -8,5 +8,18 @@ namespace MegaDesk_4_Jean_Oliveira
 {
     class Desk
     {
+        public int width { get; set; }
+        public int depth { get; set; }
+        public int numDrawers { get; set; }
+        public DesktopMaterial material { get; set; }
     }
+
+    public enum DesktopMaterial
+    {
+        Pine = 50,
+        Laminate = 100,
+        Oak = 200,
+        Rosewood = 300,
+        Veneer = 150
+    };
 }
