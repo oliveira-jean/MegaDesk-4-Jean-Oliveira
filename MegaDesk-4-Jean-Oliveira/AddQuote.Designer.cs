4437ee6430a9862724cb2fd00489e2e0ec7c9ca0
Jean Oliveira <jeanoliveiracit@gmail.com>
2017-10-07 22:21:12 -0300

    Finished the Forms

--- MegaDesk-4-Jean-Oliveira/AddQuote.Designer.cs
+++ MegaDesk-4-Jean-Oliveira/AddQuote.Designer.cs
@@ -29,10 +29,221 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.addQuoteFormLabel = new System.Windows.Forms.Label();
+            this.widthInputLabel = new System.Windows.Forms.Label();
+            this.widthTextBox = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.depthTextBox = new System.Windows.Forms.TextBox();
+            this.depthInputLabel = new System.Windows.Forms.Label();
+            this.drawersSelectLabel = new System.Windows.Forms.Label();
+            this.drawersComboBox = new System.Windows.Forms.ComboBox();
+            this.materialsComboBox = new System.Windows.Forms.ComboBox();
+            this.materialsSelectLabel = new System.Windows.Forms.Label();
+            this.noneRadioButton = new System.Windows.Forms.RadioButton();
+            this.threeRadioButton = new System.Windows.Forms.RadioButton();
+            this.fiveRadioButton = new System.Windows.Forms.RadioButton();
+            this.sevenRadioButton = new System.Windows.Forms.RadioButton();
+            this.rushOrderLabel = new System.Windows.Forms.Label();
+            this.submitOrderButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addQuoteFormLabel
+            // 
+            this.addQuoteFormLabel.AutoSize = true;
+            this.addQuoteFormLabel.Location = new System.Drawing.Point(191, 9);
+            this.addQuoteFormLabel.Name = "addQuoteFormLabel";
+            this.addQuoteFormLabel.Size = new System.Drawing.Size(86, 13);
+            this.addQuoteFormLabel.TabIndex = 0;
+            this.addQuoteFormLabel.Text = "Add Desk Quote";
+            this.addQuoteFormLabel.Click += new System.EventHandler(this.AddQuoteLabel_Click);
+            // 
+            // widthInputLabel
+            // 
+            this.widthInputLabel.AutoSize = true;
+            this.widthInputLabel.Location = new System.Drawing.Point(12, 39);
+            this.widthInputLabel.Name = "widthInputLabel";
+            this.widthInputLabel.Size = new System.Drawing.Size(276, 13);
+            this.widthInputLabel.TabIndex = 1;
+            this.widthInputLabel.Text = "Enter the WIDTH of the desk in inches( 24\" min; 96 max)";
+            this.widthInputLabel.Click += new System.EventHandler(this.WidthInputLabel_Click);
+            // 
+            // widthTextBox
+            // 
+            this.widthTextBox.Location = new System.Drawing.Point(15, 55);
+            this.widthTextBox.Name = "widthTextBox";
+            this.widthTextBox.Size = new System.Drawing.Size(100, 20);
+            this.widthTextBox.TabIndex = 1;
+            this.widthTextBox.TextChanged += new System.EventHandler(this.WidthTextBox_TextChanged);
+            // 
+            // depthTextBox
+            // 
+            this.depthTextBox.Location = new System.Drawing.Point(15, 108);
+            this.depthTextBox.Name = "depthTextBox";
+            this.depthTextBox.Size = new System.Drawing.Size(100, 20);
+            this.depthTextBox.TabIndex = 2;
+            this.depthTextBox.TextChanged += new System.EventHandler(this.DepthTextBox_TextChanged);
+            // 
+            // depthInputLabel
+            // 
+            this.depthInputLabel.AutoSize = true;
+            this.depthInputLabel.Location = new System.Drawing.Point(12, 92);
+            this.depthInputLabel.Name = "depthInputLabel";
+            this.depthInputLabel.Size = new System.Drawing.Size(276, 13);
+            this.depthInputLabel.TabIndex = 3;
+            this.depthInputLabel.Text = "Enter the DEPTH of the desk in inches( 12\" min; 56 max)";
+            this.depthInputLabel.Click += new System.EventHandler(this.DepthInputLabel_Click);
+            // 
+            // drawersSelectLabel
+            // 
+            this.drawersSelectLabel.AutoSize = true;
+            this.drawersSelectLabel.Location = new System.Drawing.Point(12, 145);
+            this.drawersSelectLabel.Name = "drawersSelectLabel";
+            this.drawersSelectLabel.Size = new System.Drawing.Size(164, 13);
+            this.drawersSelectLabel.TabIndex = 5;
+            this.drawersSelectLabel.Text = "Select the number of DRAWERS";
+            this.drawersSelectLabel.Click += new System.EventHandler(this.DrawersSelectLabel_Click);
+            // 
+            // drawersComboBox
+            // 
+            this.drawersComboBox.FormattingEnabled = true;
+            this.drawersComboBox.Location = new System.Drawing.Point(15, 161);
+            this.drawersComboBox.Name = "drawersComboBox";
+            this.drawersComboBox.Size = new System.Drawing.Size(121, 21);
+            this.drawersComboBox.TabIndex = 3;
+            this.drawersComboBox.SelectedIndexChanged += new System.EventHandler(this.DrawersComboBox_SelectedIndexChanged);
+            // 
+            // materialsComboBox
+            // 
+            this.materialsComboBox.FormattingEnabled = true;
+            this.materialsComboBox.Location = new System.Drawing.Point(15, 212);
+            this.materialsComboBox.Name = "materialsComboBox";
+            this.materialsComboBox.Size = new System.Drawing.Size(121, 21);
+            this.materialsComboBox.TabIndex = 4;
+            this.materialsComboBox.SelectedIndexChanged += new System.EventHandler(this.MaterialsComboBox_SelectedIndexChanged);
+            // 
+            // materialsSelectLabel
+            // 
+            this.materialsSelectLabel.AutoSize = true;
+            this.materialsSelectLabel.Location = new System.Drawing.Point(12, 196);
+            this.materialsSelectLabel.Name = "materialsSelectLabel";
+            this.materialsSelectLabel.Size = new System.Drawing.Size(160, 13);
+            this.materialsSelectLabel.TabIndex = 7;
+            this.materialsSelectLabel.Text = "Select the desktop MATERIALS";
+            this.materialsSelectLabel.Click += new System.EventHandler(this.MaterialsSelectLabel_Click);
+            // 
+            // noneRadioButton
+            // 
+            this.noneRadioButton.AutoSize = true;
+            this.noneRadioButton.Location = new System.Drawing.Point(15, 270);
+            this.noneRadioButton.Name = "noneRadioButton";
+            this.noneRadioButton.Size = new System.Drawing.Size(49, 17);
+            this.noneRadioButton.TabIndex = 5;
+            this.noneRadioButton.TabStop = true;
+            this.noneRadioButton.Text = "none";
+            this.noneRadioButton.UseVisualStyleBackColor = true;
+            this.noneRadioButton.CheckedChanged += new System.EventHandler(this.NoneRadioButton_CheckedChanged);
+            // 
+            // threeRadioButton
+            // 
+            this.threeRadioButton.AutoSize = true;
+            this.threeRadioButton.Location = new System.Drawing.Point(70, 270);
+            this.threeRadioButton.Name = "threeRadioButton";
+            this.threeRadioButton.Size = new System.Drawing.Size(56, 17);
+            this.threeRadioButton.TabIndex = 6;
+            this.threeRadioButton.TabStop = true;
+            this.threeRadioButton.Text = "3 days";
+            this.threeRadioButton.UseVisualStyleBackColor = true;
+            this.threeRadioButton.CheckedChanged += new System.EventHandler(this.ThreeRadioButton_CheckedChanged);
+            // 
+            // fiveRadioButton
+            // 
+            this.fiveRadioButton.AutoSize = true;
+            this.fiveRadioButton.Location = new System.Drawing.Point(132, 270);
+            this.fiveRadioButton.Name = "fiveRadioButton";
+            this.fiveRadioButton.Size = new System.Drawing.Size(56, 17);
+            this.fiveRadioButton.TabIndex = 7;
+            this.fiveRadioButton.TabStop = true;
+            this.fiveRadioButton.Text = "5 days";
+            this.fiveRadioButton.UseVisualStyleBackColor = true;
+            this.fiveRadioButton.CheckedChanged += new System.EventHandler(this.FiveRadioButton_CheckedChanged);
+            // 
+            // sevenRadioButton
+            // 
+            this.sevenRadioButton.AutoSize = true;
+            this.sevenRadioButton.Location = new System.Drawing.Point(194, 270);
+            this.sevenRadioButton.Name = "sevenRadioButton";
+            this.sevenRadioButton.Size = new System.Drawing.Size(56, 17);
+            this.sevenRadioButton.TabIndex = 8;
+            this.sevenRadioButton.TabStop = true;
+            this.sevenRadioButton.Text = "7 days";
+            this.sevenRadioButton.UseVisualStyleBackColor = true;
+            this.sevenRadioButton.CheckedChanged += new System.EventHandler(this.SevenRadioButton_CheckedChanged);
+            // 
+            // rushOrderLabel
+            // 
+            this.rushOrderLabel.AutoSize = true;
+            this.rushOrderLabel.Location = new System.Drawing.Point(12, 254);
+            this.rushOrderLabel.Name = "rushOrderLabel";
+            this.rushOrderLabel.Size = new System.Drawing.Size(61, 13);
+            this.rushOrderLabel.TabIndex = 13;
+            this.rushOrderLabel.Text = "Rush Order";
+            this.rushOrderLabel.Click += new System.EventHandler(this.RushOrderLabel_Click);
+            // 
+            // submitOrderButton
+            // 
+            this.submitOrderButton.AutoSize = true;
+            this.submitOrderButton.Location = new System.Drawing.Point(194, 325);
+            this.submitOrderButton.Name = "submitOrderButton";
+            this.submitOrderButton.Size = new System.Drawing.Size(78, 23);
+            this.submitOrderButton.TabIndex = 9;
+            this.submitOrderButton.Text = "Submit Order";
+            this.submitOrderButton.UseVisualStyleBackColor = true;
+            this.submitOrderButton.Click += new System.EventHandler(this.SubmitOrderButton_Click);
+            // 
+            // AddQuote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 388);
+            this.Controls.Add(this.submitOrderButton);
+            this.Controls.Add(this.rushOrderLabel);
+            this.Controls.Add(this.sevenRadioButton);
+            this.Controls.Add(this.fiveRadioButton);
+            this.Controls.Add(this.threeRadioButton);
+            this.Controls.Add(this.noneRadioButton);
+            this.Controls.Add(this.materialsComboBox);
+            this.Controls.Add(this.materialsSelectLabel);
+            this.Controls.Add(this.drawersComboBox);
+            this.Controls.Add(this.drawersSelectLabel);
+            this.Controls.Add(this.depthTextBox);
+            this.Controls.Add(this.depthInputLabel);
+            this.Controls.Add(this.widthTextBox);
+            this.Controls.Add(this.widthInputLabel);
+            this.Controls.Add(this.addQuoteFormLabel);
+            this.Name = "AddQuote";
             this.Text = "AddQuote";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label addQuoteFormLabel;
+        private System.Windows.Forms.Label widthInputLabel;
+        private System.Windows.Forms.TextBox widthTextBox;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox depthTextBox;
+        private System.Windows.Forms.Label depthInputLabel;
+        private System.Windows.Forms.Label drawersSelectLabel;
+        private System.Windows.Forms.ComboBox drawersComboBox;
+        private System.Windows.Forms.ComboBox materialsComboBox;
+        private System.Windows.Forms.Label materialsSelectLabel;
+        private System.Windows.Forms.RadioButton noneRadioButton;
+        private System.Windows.Forms.RadioButton threeRadioButton;
+        private System.Windows.Forms.RadioButton fiveRadioButton;
+        private System.Windows.Forms.RadioButton sevenRadioButton;
+        private System.Windows.Forms.Label rushOrderLabel;
+        private System.Windows.Forms.Button submitOrderButton;
     }
 }
---
+++ MegaDesk-4-Jean-Oliveira/DeskOrderView.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace MegaDesk_4_Jean_Oliveira
+{
+    partial class DeskOrderView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.QuoteDateLabel = new System.Windows.Forms.Label();
+            this.DeskWidthLabel = new System.Windows.Forms.Label();
+            this.DeskDepthLabel = new System.Windows.Forms.Label();
+            this.NumberOfDrawersLabel = new System.Windows.Forms.Label();
+            this.DesktopMaterialLabel = new System.Windows.Forms.Label();
+            this.RushOrderLabel = new System.Windows.Forms.Label();
+            this.QuoteAmountLabel = new System.Windows.Forms.Label();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.QuoteDateOrderViewBox = new System.Windows.Forms.TextBox();
+            this.WidthOrderViewBox = new System.Windows.Forms.TextBox();
+            this.DepthOrderViewBox = new System.Windows.Forms.TextBox();
+            this.NumberOfDrawersOrderViewBox = new System.Windows.Forms.TextBox();
+            this.DesktopMaterialOrderViewBox = new System.Windows.Forms.TextBox();
+            this.RushDaysOrderViewBox = new System.Windows.Forms.TextBox();
+            this.QuoteAmountOrderViewBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // QuoteDateLabel
+            // 
+            this.QuoteDateLabel.AutoSize = true;
+            this.QuoteDateLabel.Location = new System.Drawing.Point(12, 36);
+            this.QuoteDateLabel.Name = "QuoteDateLabel";
+            this.QuoteDateLabel.Size = new System.Drawing.Size(65, 13);
+            this.QuoteDateLabel.TabIndex = 0;
+            this.QuoteDateLabel.Text = "Quote Date:";
+            this.QuoteDateLabel.Click += new System.EventHandler(this.QuoteDateLabel_Click);
+            // 
+            // DeskWidthLabel
+            // 
+            this.DeskWidthLabel.AutoSize = true;
+            this.DeskWidthLabel.Location = new System.Drawing.Point(11, 67);
+            this.DeskWidthLabel.Name = "DeskWidthLabel";
+            this.DeskWidthLabel.Size = new System.Drawing.Size(66, 13);
+            this.DeskWidthLabel.TabIndex = 1;
+            this.DeskWidthLabel.Text = "Desk Width:";
+            this.DeskWidthLabel.Click += new System.EventHandler(this.DeskWidthLabel_Click);
+            // 
+            // DeskDepthLabel
+            // 
+            this.DeskDepthLabel.AutoSize = true;
+            this.DeskDepthLabel.Location = new System.Drawing.Point(12, 97);
+            this.DeskDepthLabel.Name = "DeskDepthLabel";
+            this.DeskDepthLabel.Size = new System.Drawing.Size(64, 13);
+            this.DeskDepthLabel.TabIndex = 2;
+            this.DeskDepthLabel.Text = "Desk Depth";
+            this.DeskDepthLabel.Click += new System.EventHandler(this.DeskDepthLabel_Click);
+            // 
+            // NumberOfDrawersLabel
+            // 
+            this.NumberOfDrawersLabel.AutoSize = true;
+            this.NumberOfDrawersLabel.Location = new System.Drawing.Point(12, 129);
+            this.NumberOfDrawersLabel.Name = "NumberOfDrawersLabel";
+            this.NumberOfDrawersLabel.Size = new System.Drawing.Size(101, 13);
+            this.NumberOfDrawersLabel.TabIndex = 3;
+            this.NumberOfDrawersLabel.Text = "Number of Drawers:";
+            this.NumberOfDrawersLabel.Click += new System.EventHandler(this.NumberOfDrawersLabel_Click);
+            // 
+            // DesktopMaterialLabel
+            // 
+            this.DesktopMaterialLabel.AutoSize = true;
+            this.DesktopMaterialLabel.Location = new System.Drawing.Point(12, 163);
+            this.DesktopMaterialLabel.Name = "DesktopMaterialLabel";
+            this.DesktopMaterialLabel.Size = new System.Drawing.Size(90, 13);
+            this.DesktopMaterialLabel.TabIndex = 4;
+            this.DesktopMaterialLabel.Text = "Desktop Material:";
+            this.DesktopMaterialLabel.Click += new System.EventHandler(this.DesktopMaterialLabel_Click);
+            // 
+            // RushOrderLabel
+            // 
+            this.RushOrderLabel.AutoSize = true;
+            this.RushOrderLabel.Location = new System.Drawing.Point(12, 193);
+            this.RushOrderLabel.Name = "RushOrderLabel";
+            this.RushOrderLabel.Size = new System.Drawing.Size(97, 13);
+            this.RushOrderLabel.TabIndex = 5;
+            this.RushOrderLabel.Text = "Rush Order (Days):";
+            this.RushOrderLabel.Click += new System.EventHandler(this.RushOrderLabel_Click);
+            // 
+            // QuoteAmountLabel
+            // 
+            this.QuoteAmountLabel.AutoSize = true;
+            this.QuoteAmountLabel.Location = new System.Drawing.Point(12, 224);
+            this.QuoteAmountLabel.Name = "QuoteAmountLabel";
+            this.QuoteAmountLabel.Size = new System.Drawing.Size(75, 13);
+            this.QuoteAmountLabel.TabIndex = 6;
+            this.QuoteAmountLabel.Text = "Quote Amount";
+            this.QuoteAmountLabel.Click += new System.EventHandler(this.QuoteAmountLabel_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(100, 278);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 7;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            // 
+            // QuoteDateOrderViewBox
+            // 
+            this.QuoteDateOrderViewBox.Location = new System.Drawing.Point(144, 29);
+            this.QuoteDateOrderViewBox.Name = "QuoteDateOrderViewBox";
+            this.QuoteDateOrderViewBox.ReadOnly = true;
+            this.QuoteDateOrderViewBox.Size = new System.Drawing.Size(100, 20);
+            this.QuoteDateOrderViewBox.TabIndex = 8;
+            this.QuoteDateOrderViewBox.TextChanged += new System.EventHandler(this.QuoteDateOrderViewBox_TextChanged);
+            // 
+            // WidthOrderViewBox
+            // 
+            this.WidthOrderViewBox.Location = new System.Drawing.Point(144, 60);
+            this.WidthOrderViewBox.Name = "WidthOrderViewBox";
+            this.WidthOrderViewBox.ReadOnly = true;
+            this.WidthOrderViewBox.Size = new System.Drawing.Size(100, 20);
+            this.WidthOrderViewBox.TabIndex = 9;
+            // 
+            // DepthOrderViewBox
+            // 
+            this.DepthOrderViewBox.Location = new System.Drawing.Point(144, 90);
+            this.DepthOrderViewBox.Name = "DepthOrderViewBox";
+            this.DepthOrderViewBox.ReadOnly = true;
+            this.DepthOrderViewBox.Size = new System.Drawing.Size(100, 20);
+            this.DepthOrderViewBox.TabIndex = 10;
+            this.DepthOrderViewBox.TextChanged += new System.EventHandler(this.DepthOrderViewBox_TextChanged);
+            // 
+            // NumberOfDrawersOrderViewBox
+            // 
+            this.NumberOfDrawersOrderViewBox.Location = new System.Drawing.Point(144, 122);
+            this.NumberOfDrawersOrderViewBox.Name = "NumberOfDrawersOrderViewBox";
+            this.NumberOfDrawersOrderViewBox.ReadOnly = true;
+            this.NumberOfDrawersOrderViewBox.Size = new System.Drawing.Size(100, 20);
+            this.NumberOfDrawersOrderViewBox.TabIndex = 11;
+            // 
+            // DesktopMaterialOrderViewBox
+            // 
+            this.DesktopMaterialOrderViewBox.Location = new System.Drawing.Point(144, 156);
+            this.DesktopMaterialOrderViewBox.Name = "DesktopMaterialOrderViewBox";
+            this.DesktopMaterialOrderViewBox.ReadOnly = true;
+            this.DesktopMaterialOrderViewBox.Size = new System.Drawing.Size(100, 20);
+            this.DesktopMaterialOrderViewBox.TabIndex = 12;
+            // 
+            // RushDaysOrderViewBox
+            // 
+            this.RushDaysOrderViewBox.Location = new System.Drawing.Point(144, 186);
+            this.RushDaysOrderViewBox.Name = "RushDaysOrderViewBox";
+            this.RushDaysOrderViewBox.ReadOnly = true;
+            this.RushDaysOrderViewBox.Size = new System.Drawing.Size(100, 20);
+            this.RushDaysOrderViewBox.TabIndex = 13;
+            // 
+            // QuoteAmountOrderViewBox
+            // 
+            this.QuoteAmountOrderViewBox.Location = new System.Drawing.Point(144, 217);
+            this.QuoteAmountOrderViewBox.Name = "QuoteAmountOrderViewBox";
+            this.QuoteAmountOrderViewBox.ReadOnly = true;
+            this.QuoteAmountOrderViewBox.Size = new System.Drawing.Size(100, 20);
+            this.QuoteAmountOrderViewBox.TabIndex = 14;
+            // 
+            // DeskOrderView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 313);
+            this.Controls.Add(this.QuoteAmountOrderViewBox);
+            this.Controls.Add(this.RushDaysOrderViewBox);
+            this.Controls.Add(this.DesktopMaterialOrderViewBox);
+            this.Controls.Add(this.NumberOfDrawersOrderViewBox);
+            this.Controls.Add(this.DepthOrderViewBox);
+            this.Controls.Add(this.WidthOrderViewBox);
+            this.Controls.Add(this.QuoteDateOrderViewBox);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.QuoteAmountLabel);
+            this.Controls.Add(this.RushOrderLabel);
+            this.Controls.Add(this.DesktopMaterialLabel);
+            this.Controls.Add(this.NumberOfDrawersLabel);
+            this.Controls.Add(this.DeskDepthLabel);
+            this.Controls.Add(this.DeskWidthLabel);
+            this.Controls.Add(this.QuoteDateLabel);
+            this.Name = "DeskOrderView";
+            this.Text = "DeskOrderView";
+            this.Load += new System.EventHandler(this.DeskOrderView_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label QuoteDateLabel;
+        private System.Windows.Forms.Label DeskWidthLabel;
+        private System.Windows.Forms.Label DeskDepthLabel;
+        private System.Windows.Forms.Label NumberOfDrawersLabel;
+        private System.Windows.Forms.Label DesktopMaterialLabel;
+        private System.Windows.Forms.Label RushOrderLabel;
+        private System.Windows.Forms.Label QuoteAmountLabel;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.TextBox QuoteDateOrderViewBox;
+        private System.Windows.Forms.TextBox WidthOrderViewBox;
+        private System.Windows.Forms.TextBox DepthOrderViewBox;
+        private System.Windows.Forms.TextBox NumberOfDrawersOrderViewBox;
+        private System.Windows.Forms.TextBox DesktopMaterialOrderViewBox;
+        private System.Windows.Forms.TextBox RushDaysOrderViewBox;
+        private System.Windows.Forms.TextBox QuoteAmountOrderViewBox;
+    }
+}
